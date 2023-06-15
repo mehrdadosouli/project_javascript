97ff7ce00c09f0256a22ab16c345f60d4397b3ff
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2023-06-15 10:08:48 +0330

    import folder cms html css js in project

---
+++ FrontEnd/panel/shared.js
@@ -0,0 +1,13 @@
+import { getAdminInfos } from "./funcs/utils.js";
+
+const $ = document
+
+window.addEventListener('load', () => {
+    const adminWelcomeNameElem = $.querySelector('#admin-welcome-name')
+    const adminNameElem = $.querySelector('#admin-name')
+    getAdminInfos().then(admin => {
+        console.log(admin);
+        adminNameElem.innerHTML = admin.name
+        adminWelcomeNameElem.innerHTML = admin.name
+    })
+})
