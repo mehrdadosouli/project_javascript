ce5bd54e22935aea6e273eb4e845fd037d366ed9
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2023-06-16 15:12:18 +0330

    set filtered notification when you select see and then  the notification has hiiden online

--- FrontEnd/js/panel/funcs/notification.js
+++ FrontEnd/js/panel/funcs/notification.js
@@ -1,37 +1,43 @@
 import { getToken } from "../../funcs/utils.js"
 
 
+const notificationModalListElem = document.querySelector('.home-notification-modal-list')
 const notificationalertbox=(notifications)=>{
     
-    const notificationModalListElem = document.querySelector('.home-notification-modal-list')
     if (notifications.length) {
         notifications.forEach(notification => {
           notificationModalListElem.insertAdjacentHTML('beforeend', `
               <li class="home-notification-modal-item">
                   <span class="home-notification-modal-text">${notification.msg}</span>
-                  <a onclick="seeNotification('${notification._id}')">دیدم</a>
+                  <a onclick='seeNotification(${JSON.stringify(notifications)},${JSON.stringify(notification._id)})'>دیدم</a>
               </li>
           `)
         })
       } else {
           notificationModalListElem.insertAdjacentHTML('beforeend', `
-          <li class="home-notification-modal-item">
+          <li style="background-color:green" class="home-notification-modal-item">
               <span class="home-notification-modal-text">پیغامی نیست</span>
-              <a >دیدم</a>
           </li>
       `)
       }
 
     }
-    const seeNotification=async(notificationsid)=>{
+    const seeNotification=async(notifications,notificationsid)=>{
       const res = await fetch(`http://localhost:4000/v1/notifications/see/${notificationsid}`,{
       method:'PUT',
       headers:{
           Authorization:`Bearer ${getToken()}`
       }
-    })
+    });
+    filteredNotification(notifications,notificationsid);
     const data=await res.json();
     console.log(data);
+    }
+
+    const filteredNotification=(notifications,notificationsid)=>{
+        notificationModalListElem.innerHTML="";
+       const resultFiltered= notifications.filter(elem=>elem._id !== notificationsid);
+       notificationalertbox(resultFiltered)
     }
 
 export { notificationalertbox ,seeNotification}
