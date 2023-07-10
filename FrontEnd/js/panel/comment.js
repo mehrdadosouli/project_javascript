8d96d50e7095ca8698e160e963d32433eede2d60
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2023-07-10 21:27:37 +0330

    set delete btn and reject btn fore comments.html

--- FrontEnd/js/panel/comment.js
+++ FrontEnd/js/panel/comment.js
@@ -1,5 +1,9 @@
-import { getAndRenderComments ,showComments} from "./funcs/comments.js"
+import { getAndRenderComments ,showComments ,answerComments ,acceptBtn , rejectBtn , deleteBtn} from "./funcs/comments.js"
 window.showComments=showComments
+window.acceptBtn=acceptBtn
+window.rejectBtn=rejectBtn
+window.deleteBtn=deleteBtn
+window.answerComments=answerComments
 window.addEventListener('load',()=>{
     getAndRenderComments()
 })
--- FrontEnd/js/panel/funcs/comments.js
+++ FrontEnd/js/panel/funcs/comments.js
@@ -1,11 +1,10 @@
-import { showswall } from "../../../js/funcs/utils.js";
+import { getToken, showswall } from "../../../js/funcs/utils.js";
 let table=document.querySelector('.table tbody')
 const getAndRenderComments=async()=>{
     const res=await fetch('http://localhost:4000/v1/comments')
     const data=await res.json()
     table.innerHTML=""
     data.forEach((comment,index)=>{
-        console.log(comment);
         table.insertAdjacentHTML('beforeend',`
         <tr>
         <td style="${comment.answer? "background-color:green" : "background-color:red"}">${index+1}</td>
@@ -17,10 +16,10 @@ const getAndRenderComments=async()=>{
              <button type="button" onclick="showComments('${comment.body}')" class="btn btn-success" id="show-btn">مشاهده</button>               
         </td>
         <td>
-             <button type='button' onclick='answerComments()' class='btn btn-primary answer-btn'>پاسخ</button>
+             <button type='button' onclick="answerComments('${comment._id}')" class='btn btn-primary answer-btn'>پاسخ</button>
         </td>
         <td>
-             <button type="button" onclick="aceptBtn('${comment._id}')" class="btn btn-success" id="delete-btn">تایید </button>
+             <button type="button" onclick="acceptBtn('${comment._id}')" class="btn btn-success" id="delete-btn">تایید </button>
         </td>
         <td>
              <button type="button" onclick="rejectBtn('${comment._id}')" class="btn btn-danger" id="delete-btn">رد </button>
@@ -41,4 +40,72 @@ const showComments=(txt)=>{
       ,()=>{})
 }
 
-export{ getAndRenderComments ,showComments }
+const answerComments=async(id)=>{
+    
+    swal({
+        title:"answer",
+         content:"input",
+         buttons:"ok"
+    }).then(async(result)=>{
+        console.log(result);
+        let bodys={
+            body:result,
+            score:5
+        }
+        const res=await fetch(`http://localhost:4000/v1/comments/answer/${id}`,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization:`Bearer ${getToken()}`
+            },
+            body:JSON.stringify(bodys)
+        })
+        if(res.ok){
+            showswall("با موفقیت ارسال شد","success","ok",()=>{
+                getAndRenderComments()
+            })
+        }
+    })
+}
+
+const acceptBtn=async(id)=>{
+    const res=await fetch(`http://localhost:4000/v1/comments/accept/${id}`,{
+        method:"PUT",
+        headers:{
+            Authorization:`Bearer ${getToken()}`
+        },
+    })
+    if(res.ok){
+        showswall("کامنت با موفقیت ثبت شد","success","ok",()=>{
+            getAndRenderComments()
+        })
+    }
+}
+const rejectBtn=async(id)=>{
+    const res=await fetch(`http://localhost:4000/v1/comments/reject/${id}`,{
+        method:"PUT",
+        headers:{
+            Authorization:`Bearer ${getToken()}`
+        },
+    })
+    if(res.ok){
+        showswall("کامنت با موفقیت ریجکت شد","success","ok",()=>{
+            getAndRenderComments()
+        })
+    }
+}
+const deleteBtn=async(id)=>{
+    const res=await fetch(`http://localhost:4000/v1/comments/${id}`,{
+        method:"DELETE",
+        headers:{
+            Authorization:`Bearer ${getToken()}`
+        },
+    })
+    if(res.ok){
+        showswall("کامنت با موفقیت حذف شد","success","ok",()=>{
+            getAndRenderComments()
+        })
+    }
+}
+
+export{ getAndRenderComments ,showComments ,answerComments ,acceptBtn , rejectBtn , deleteBtn}
